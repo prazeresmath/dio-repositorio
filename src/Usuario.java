87d6055cdef6eccb2ee07075385a9090ba97dfab
MatheusPS <106271268+prazeresmath@users.noreply.github.com>
2023-03-09 12:46:07 -0300

    Create Usuario.java

---
+++ src/Usuario.java
@@ -0,0 +1,24 @@
+public class Usuario {
+    
+    public static void main(String[] args) throws Exception {
+        
+        SmartTv smartTv = new SmartTv();
+        
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+        
+        System.out.println("Canal atual ?" + smartTv.canal);
+        
+        smartTv.mudarCanal(13);
+        System.out.println("Volume atual : " +smartTv.volume);
+        
+        System.out.println("TV Ligada ?" + smartTv.ligada);
+        
+        System.out.println("Volume atual ?" + smartTv.volume);
+        
+        smartTv.ligar();
+        System.out.println("Novo status -> TV Ligada ?" + smartTv.ligada);
+    }
+}
